50d6ebba8ee3fc211da95993e536a39c4f565050
jmmalouf@go.olemiss.edu <jmmalouf@go.olemiss.edu>
2018-04-25 17:21:02 -0500

    changed space/tabs on board_state, added defaults. Expand is now a few functions not a class

--- board_class.py
+++ board_class.py
@@ -1,47 +1,45 @@
 class BoardState:
 
- def __init__(self, board, turn, p1_flips, p2_flips):
- 	
- 	if board == None:
- 		board_create()
- 	else:
- 		self.board = board
- 	self.flipped = False
- 	self.player_turn = turn
- 	self.p1_flips = p1_flips
- 	self.p2_flips = p2_flips
+    def __init__(self, board=None, turn=1, p1_flips=4, p2_flips=4):
 
- def board_create(self):
- 	self.board = []
- 	for i in range(7):
- 		self.board.append([0,0,0,0,0,0])
+        if board is None:
+            self.board_create()
+        else:
+            self.board = board
+        self.flipped = False
+        self.player_turn = turn
+        self.p1_flips = p1_flips
+        self.p2_flips = p2_flips
 
+    def board_create(self):
+        self.board = []
+        for i in range(7):
+            self.board.append([0, 0, 0, 0, 0, 0])
 
- def place_piece(self, pos, player):
- 	placed = False
- 	for i in range(6):
- 		if self.board[pos][i] == 0:
- 			self.board[pos][i] = player
- 			self.flipped = False
- 			placed = True
- 			break
+    def place_piece(self, pos, player):
+        placed = False
+        for i in range(6):
+            if self.board[pos][i] == 0:
+                self.board[pos][i] = player
+                self.flipped = False
+                placed = True
+                break
 
- 	return placed
+        self.flipped = False
+        return placed
 
+    def print_board(self):
+        print(self.board)
 
- def print_board(self):
- 	print(self.board)
+    def flip_board(self):
+        for i in self.board:
+            temp_col = []
+            for j in i:
+                if j != 0:
+                    temp_col.append(j)
+            temp_col.reverse()
+            for j in range(len(temp_col)):
+                if i[j] != 0:
+                    i[j] = temp_col[j]
 
- def flip_board(self):
- 	for i in self.board:
- 		temp_col = []
- 		for j in i:
- 			if j != 0:
- 				temp_col.append(j)
- 		temp_col.reverse()
- 		for j in range(len(temp_col)):
- 			if i[j] != 0:
- 				i[j] = temp_col[j]
-
-
- 	self.flipped = True
+        self.flipped = True
--- expand.py
+++ expand.py
@@ -1,44 +1,51 @@
+from board_class import BoardState
 from copy import deepcopy
 
 
-class Expand:
+def breadth(board_state):
+    flipped_board = flip(deepcopy(board_state.board))
+    for i in range(7):
+        for j in range(6):
+            if board_state.board[i][j] == 0:
+                board_state.board[i][j] = board_state.player_turn
+                yield(board_state.print_board())
+                board_state.board[i][j] = 0
+                if j < 6 and not board_state.flipped:
+                    for k in range(j, 0, -1):
+                        board_state.board[i][k] = board_state.board[i][k-1]
+                    board_state.board[i][0] = board_state.player_turn
+                    yield(board_state.print_board())
+                    for k in range(j):
+                        board_state.board[i][k] = board_state.board[i][k+1]
+                    board_state.board[i][j] = 0
+                break
+    for i in range(7):
+        for j in range(6):
+            if flipped_board[i][j] == 0:
+                if not board_state.flipped:
+                    flipped_board[i][j] = board_state.player_turn
+                    yield(BoardState(flipped_board).print_board())
+                    flipped_board[i][j] = 0
+                if j < 6:
+                    for k in range(j, 0, -1):
+                        flipped_board[i][k] = flipped_board[i][k-1]
+                    flipped_board[i][0] = board_state.player_turn
+                    yield(BoardState(flipped_board).print_board())
+                    for k in range(j):
+                        flipped_board[i][k] = flipped_board[i][k+1]
+                    flipped_board[i][j] = 0
+                break
 
-    def __init__(self, board_state, player, end_flip):
-        self.board = deepcopy(board_state.board)
-        board_state.flip_board()
-        self.flipped_board = deepcopy(board_state.board)
-        self.player = player
-        self.end_flip = end_flip
 
-    def breadth(self):
-        for i in range(7):
-            for j in range(6):
-                if self.board[i][j] == 0:
-                    self.board[i][j] = self.player
-                    yield(self.board)
-                    self.board[i][j] = 0
-                    if j < 6 and not self.end_flip:
-                        for k in range(j, 0, -1):
-                            self.board[i][k] = self.board[i][k-1]
-                        self.board[i][0] = self.player
-                        yield(self.board)
-                        for k in range(j):
-                            self.board[i][k] = self.board[i][k+1]
-                        self.board[i][j] = 0
-                    break
-        for i in range(7):
-            for j in range(6):
-                if self.flipped_board[i][j] == 0:
-                    if not self.end_flip:
-                        self.flipped_board[i][j] = self.player
-                        yield(self.flipped_board)
-                        self.board[i][j] = 0
-                    if j < 6:
-                        for k in range(j, 0, -1):
-                            self.flipped_board[i][k] = self.flipped_board[i][k-1]
-                        self.flipped_board[i][0] = self.player
-                        yield (self.flipped_board)
-                        for k in range(j):
-                            self.flipped_board[i][k] = self.flipped_board[i][k+1]
-                        self.flipped_board[i][j] = 0
-                    break
+def flip(board):
+        for i in board:
+            temp_col = []
+            for j in i:
+                if j != 0:
+                    temp_col.append(j)
+            temp_col.reverse()
+            for j in range(len(temp_col)):
+                if i[j] != 0:
+                    i[j] = temp_col[j]
+        return board
+
