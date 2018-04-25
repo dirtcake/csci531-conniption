918cc2b9082f206cacb675d3780eba0ad8dc9b39
jmmalouf@go.olemiss.edu <jmmalouf@go.olemiss.edu>
2018-04-25 16:30:54 -0500

    deleted test code in expand.py

--- expand.py
+++ expand.py
@@ -1,6 +1,4 @@
-from board_class import BoardState
 from copy import deepcopy
-import time
 
 
 class Expand:
@@ -17,13 +15,13 @@ class Expand:
             for j in range(6):
                 if self.board[i][j] == 0:
                     self.board[i][j] = self.player
-                    yield(BoardState(self.board).print_board())
+                    yield(self.board)
                     self.board[i][j] = 0
                     if j < 6 and not self.end_flip:
                         for k in range(j, 0, -1):
                             self.board[i][k] = self.board[i][k-1]
                         self.board[i][0] = self.player
-                        yield(BoardState(self.board).print_board())
+                        yield(self.board)
                         for k in range(j):
                             self.board[i][k] = self.board[i][k+1]
                         self.board[i][j] = 0
@@ -33,30 +31,14 @@ class Expand:
                 if self.flipped_board[i][j] == 0:
                     if not self.end_flip:
                         self.flipped_board[i][j] = self.player
-                        yield(BoardState(self.flipped_board).print_board())
+                        yield(self.flipped_board)
                         self.board[i][j] = 0
                     if j < 6:
                         for k in range(j, 0, -1):
                             self.flipped_board[i][k] = self.flipped_board[i][k-1]
                         self.flipped_board[i][0] = self.player
-                        yield (BoardState(self.flipped_board).print_board())
+                        yield (self.flipped_board)
                         for k in range(j):
                             self.flipped_board[i][k] = self.flipped_board[i][k+1]
                         self.flipped_board[i][j] = 0
                     break
-
-
-ahh = BoardState(None)
-ahh.place_piece(1, 1)
-ahh.place_piece(1, 2)
-ahh.place_piece(1, 1)
-ahh.place_piece(1, 1)
-ahh.place_piece(1, 1)
-stuff = Expand(ahh, 1, True)
-count = 0
-start = time.time()
-for board in stuff.breadth():
-    count += 1
-print(count)
-end = time.time()
-print(end-start)
