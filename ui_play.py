ac25b087a39eee0628f0fc7d7ac118ec2fae30ee
BanquosStepson <crcummins3@gmail.com>
2018-05-08 21:04:49 -0500

    implemented random  vs ai games
    implemented random vs ai games

--- ui_play.py
+++ ui_play.py
@@ -7,7 +7,7 @@ import functools
 import random
 import sys
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk 
 
 root = Tk()
 global player_type1 
@@ -19,16 +19,18 @@ global flipping2
 global flips_get_P1 
 global flips_get_P2 
 global state
+global win_percent
 
 	
 def human_move(*args):
 	global state
 	state.print_board()
-	first_move = True
+	first_move = True 
 	if player_type1.get() == "minimax" and first_move == False:
 		state, move = functools.partial(alpha_beta_search, depth=6)
 		if evaluate_full(state.board, 1) >= 500000:
 		 	status.set("Player 1 wins")
+		 	return 1
 	
 	if state.player_turn == 1:
 		flips = state.p1_flips
@@ -61,20 +63,23 @@ def human_move(*args):
 	if player_type1.get() == "human":
 		 if evaluate_full(state.board, 1) >= 500000:
 		 	status.set("Player 1 wins")
+		 	return 1
 
 	elif player_type2.get() == "human":
 		if evaluate_full(state.board, 2) >= 500000:
 			status.set("Player 2 wins")
+			return 2
 
 	if player_type2.get() == "minimax":
 		state, move = functools.partial(alpha_beta_search, depth=6)
 		if evaluate_full(state.board, 2) >= 500000:
 			status.set("PLayer 2 wins")
+			return 2
 	state.print_board()
 
 
 	
-def human_input(*args):  #needs to be renamed, is acutally human piece entry
+def human_input(*args):  #creates window for human input
 	place = Toplevel()
 	place.title("Play Conniption")
 	game = ttk.Frame(place, padding= "4 4 12 12")
@@ -103,16 +108,60 @@ def human_input(*args):  #needs to be renamed, is acutally human piece entry
 	ttk.Button(game, text = "Place Piece", command=human_move).grid(column = 5, row= 4, sticky=(W)) #does not have the correct fuction call	
 
 def play(*args): #will fill with player
-	if player_type1 == "minimax":
-		state, move = functools.partial(alpha_beta_search, depth=6)
-
+	global state
+	state = BoardState()
 	if player_type1.get() == "human" or player_type2.get() == "human":
+		if player_type1 == "minimax":
+			state, move = functools.partial(alpha_beta_search, depth=6)
 		human_input()
-
-
+	else:
+		random_out()
 	
+def random_out(*args): #output for random vs ai games
+	rando = Toplevel()
+	rando.title("AI vs Random")
+	wind = ttk.Frame(rando, padding= "4 4 12 12")
+	wind.grid(column=0, row=0, sticky=(N, W, E, S))
+	wind.columnconfigure(0, weight=1)
+	wind.rowconfigure(0, weight=1)
+	ttk.Label(wind, text = "Win Percentage against random: ").grid(column = 1, row= 1, sticky=(W,E))
+	ttk.Label(wind, textvariable = win_percent).grid(column = 2, row = 1, sticky = (W,E))
+	rando_out()
 
-def game_mode(root):
+
+
+def rando_out(*args): #plays ai against random and returns the win percentage
+	count = 0
+	wins = 0
+	global state
+	global win_percent
+	while count < 100:
+		if player_type1.get() == "minimax":
+			state, move = functools.partial(alpha_beta_search, depth=6)
+			if evaluate_full(state.board, 1) >= 500000:
+				count += 1
+				wins += 1
+				continue
+			state.place_piece(random.randint(-1,6), state.player_turn)
+			if evaluate_full(state.board, 2) >= 500000:
+				count +=1
+				continue
+
+		if player_type2.get() == "minimax":
+			state.place_piece(random.randint(-1,6), state.player_turn)
+			if evaluate_full(state.board, 2) >= 500000:
+				count +=1
+				continue
+			state, move = functools.partial(alpha_beta_search, depth=6)
+			if evaluate_full(state.board, 2) >= 500000:
+				count += 1
+				wins += 1
+				continue
+	total = (win/count)*100
+	win_percent.set(total)
+
+
+def game_mode(root): #creates the window to pick which players play where.
 
 	root.title("Play Conniption")
 	mainframe = ttk.Frame(root, padding= "4 5 12 12")
@@ -130,7 +179,7 @@ def game_mode(root):
 	ttk.Radiobutton(mainframe, text= "Random", variable=player_type2, value="random").grid(column=3, row=3, sticky=(E,W))
 	ttk.Radiobutton(mainframe, text= "AI", variable=player_type2, value="minimax").grid(column=4, row=3, sticky=(W))
 
-
+win_percent = DoubleVar()
 state = BoardState()
 player_type1 = StringVar()
 player_type2 = StringVar()
